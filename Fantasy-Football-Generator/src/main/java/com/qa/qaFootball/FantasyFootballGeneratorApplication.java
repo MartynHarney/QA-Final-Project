4e341c2ba71c9a21bb3ee5c69281e9860e848c4e
Martyn Harney <mharney97@gmail.com>
2022-03-21 12:10:19 +0000

    new project

---
+++ Fantasy-Football-Generator/src/main/java/com/qa/qaFootball/FantasyFootballGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.qa.qaFootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFootballGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyFootballGeneratorApplication.class, args);
+	}
+
+}
